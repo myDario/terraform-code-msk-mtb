97363a7b82aeee9578a330323e802d4df7b9cf93
RishikDario <rishik@dariohealth.com>
2023-02-27 14:41:16 +0530

    new12

--- kinesis.tf
+++ kinesis.tf
@@ -1,23 +1,17 @@
-resource "aws_kinesis_stream" "this" {
-  name             = "terraform-kinesis-test"
+resource "aws_kinesis_stream" "my_stream" {
+  name             = "my-kinesis-stream"
   shard_count      = 1
   retention_period = 7
 }
-
-resource "aws_iam_policy" "put_stream" {
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "kinesis:PutRecord",
-                "kinesis:PutRecords"
-            ],
-            "Resource": "${aws_kinesis_stream.this.arn}"
-        }
-    ]
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-firehose-bucket"
+  acl    = "private"
 }
-EOF
+resource "aws_kinesis_firehose_delivery_stream" "my_delivery_stream" {
+  name               = "my-delivery-stream"
+  destination        = "s3"
+  s3_bucket_arn      = aws_s3_bucket.my_bucket.arn
+  s3_buffer_interval = 300
+  s3_buffer_size     = 5
+  depends_on         = [aws_kinesis_stream.my_stream]
 }
